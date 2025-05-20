54e16eb6cacb0fbfc37e5425ed115f1f0fadfbd1
Shreya Bingi <shreya.bingi@ting.in>
2025-05-20 21:17:45 +0530

    changes in DOM File

---
+++ 03. DOM/Starter/1. What Is DOM/app.js
@@ -0,0 +1,3 @@
+console.dir( document.head)
+console.dir( document.button)
+console.dir( document.title)
--- 03. DOM/Starter/2. Accessnig the DOM/live-coding/app.js
+++ 03. DOM/Starter/2. Accessnig the DOM/live-coding/app.js
@@ -1,9 +1,27 @@
 // ************ DOM SELECTORS ************
-// 1. getElementsByTagName
-// 2. getElementById
-// 3. getElementsByClassName
-// 4. querySelector
-// 5. querySelectorAll
+// // 1. getElementsByTagName
+// in css will use h1{
+//     style
+// }
+// console.log(document.getElementsByTagName("h1").length);
 
+
+
+// 2. getElementById
+// console.log(document.getElementById("main"));
+
+
+
+// 3. getElementsByClassName 
+// console.log(document.getElementsByClassName("cls"));
+// 4. querySelector
+// if we want to target only the first class it will not apply to other class even if it has the same class
+console.log(document.querySelectorAll("li"));
+
+// 5. querySelectorAll
+// we will use query selector all when we want that the class we want to apply changes will also afftect the divs which have the same class
+console.log(document.querySelector("li"));
 // ---------------------------------------
 // Storing data in variables
+const name = document.getElementsByTagName("h1");
+console.log(name)
